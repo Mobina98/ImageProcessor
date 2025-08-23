a8c3794a58a1c8ebd1e3c079e67fe24b79edad0e
Mobina Ahmadimasoud <st067173@student.spbu.ru>
2025-08-23 15:18:32 +0300

    Update methods.cpp

--- methods.cpp
+++ methods.cpp
@@ -42,15 +42,15 @@ Methods::Methods(const std::string &filename) {
         throw std::runtime_error("Unexpected file size.");
     }
 
-    // Переходим к данным изображения
+    
     file.seekg(header.offsetData, file.beg);
 
-    // Выделяем память построчно
+    
     if (!allocateMemory(infoHeader.height, infoHeader.width)) {
         throw std::runtime_error("Failed to allocate memory for image.");
     }
 
-    // Считываем данные построчно
+    
     for (int i = 0; i < infoHeader.height; ++i) {
         file.read(reinterpret_cast<char *>(data[i].data()), infoHeader.width * sizeof(Pixel));
         if (!file) {
@@ -62,7 +62,7 @@ Methods::Methods(const std::string &filename) {
 }
 
 Methods::~Methods() {
-    data.clear();  // Автоматически освобождается память при уничтожении контейнера
+    data.clear();  
 }
 
 void Methods::Save(const std::string &filename) {
@@ -117,34 +117,34 @@ void Methods::GaussianFilter(int kernelSize, const std::vector<std::vector<float
         throw std::invalid_argument("Kernel size must be odd and match the dimensions of the kernel matrix.");
     }
 
-    // Создаем временный буфер для хранения обработанных пикселей
+    
     std::vector<std::vector<Pixel>> tempData(infoHeader.height, std::vector<Pixel>(infoHeader.width));
 
-    int offset = kernelSize / 2; // Смещение от центра ядра
+    int offset = kernelSize / 2; 
 
     for (int y = 0; y < infoHeader.height; ++y) {
         for (int x = 0; x < infoHeader.width; ++x) {
             float sumRed = 0.0f, sumGreen = 0.0f, sumBlue = 0.0f;
 
-            // Проход по ядру свертки
+            
             for (int ky = -offset; ky <= offset; ++ky) {
                 for (int kx = -offset; kx <= offset; ++kx) {
-                    // Координаты пикселей с учетом границ изображения
+                    
                     int nx = std::clamp(x + kx, 0, infoHeader.width - 1);
                     int ny = std::clamp(y + ky, 0, infoHeader.height - 1);
 
-                    // Координаты ядра
+                    
                     int kernelX = kx + offset;
                     int kernelY = ky + offset;
 
-                    // Применение ядра
+                    
                     sumRed   += data[ny][nx].red * kernel[kernelY][kernelX];
                     sumGreen += data[ny][nx].green * kernel[kernelY][kernelX];
                     sumBlue  += data[ny][nx].blue * kernel[kernelY][kernelX];
                 }
             }
 
-            // Запись результатов с учетом допустимого диапазона значений
+            
             tempData[y][x].red   = static_cast<uint8_t>(std::clamp(sumRed, 0.0f, 255.0f));
             tempData[y][x].green = static_cast<uint8_t>(std::clamp(sumGreen, 0.0f, 255.0f));
             tempData[y][x].blue  = static_cast<uint8_t>(std::clamp(sumBlue, 0.0f, 255.0f));
